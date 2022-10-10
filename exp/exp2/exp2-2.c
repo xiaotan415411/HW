8a645e80ddcd89bcc0d517a2ee16fb0fba6751c6
xiaotan415411 <xiaotan@stu.gdou.edu.cn>
2022-10-10 15:56:01 +0800

    Merge branch 'HW' of github.com:xiaotan415411/HW into HW

---
+++ exp/exp2/exp2-2.c
@@ -0,0 +1,105 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+typedef struct SNode {
+	int data;
+	struct SNode* next;
+} SLinkNode;
+
+SLinkNode* InitSLink() {
+	SLinkNode* head = (SLinkNode*)malloc(sizeof(SLinkNode));
+	return head;
+}
+
+void CreateLink(SLinkNode* head, int* data, int amount) {
+	head->data = data[0];
+	head->next = 0;
+	SLinkNode* p = head;
+	for (int i = 1; i < amount; i++) {
+		SLinkNode* s = (SLinkNode*)malloc(sizeof(SLinkNode));
+		s->data = data[i];
+		s->next = 0;
+		p->next = s;
+		p = s;
+	}
+}
+
+//SLinkNode* DelDub(SLinkNode* head) {
+//	SLinkNode* fore = head, *patch = head;
+//	int cnt = 0,temp = 0;
+//	while (fore != 0) {
+//		temp = fore->data;
+//		cnt++;
+//		if (cnt >= 3) {
+//			patch = patch->next;
+//		}
+//		if (fore->next != 0 && fore->next->data == fore->data) {
+//			fore = fore->next;
+//			while (fore->data == patch->next->data) {
+//				if((fore = fore->next) == 0){
+//					patch->next = 0;
+//					return head;
+//				}
+//			}
+//			if (patch->next->data == head->data) {
+//				patch = head = fore;
+//			} else
+//				patch->next = fore;
+//			cnt = 1;
+//			continue;
+//		}
+//		fore = fore->next;
+//	}
+//	return head;
+//}
+
+SLinkNode* DelDub2(SLinkNode* head){
+	SLinkNode dum;
+	dum.next = head;
+	SLinkNode* pre = head,*patch = &dum;
+	int temp = 0;
+	while(pre != 0){
+		temp = pre->data;
+		if(pre->next != 0 && pre->next->data != temp){
+			patch = pre;
+			pre = pre->next;
+		}else if(pre->next == 0){
+			return dum.next;
+		}else{
+			pre = pre->next;
+			while(pre != 0 && pre->data == temp)
+				pre = pre->next;
+			patch->next = pre;
+		}
+	}
+	return dum.next;
+}
+
+void ShowList(SLinkNode* head) {
+	SLinkNode* p = head;
+	while (p != 0){
+		printf("%d ", p->data);
+		p = p->next;
+	}
+	putchar('\n');
+}
+
+#define MaxSize 9
+int data[MaxSize];
+void InitData(int* data) {
+	for (int i = 0; i < MaxSize; i++)
+		data[i] = i;
+}
+
+int main() {
+//	InitData(data);
+//	data[7] = 9;
+//	data[8] = 9;
+//	data[9] = 9;
+	int data[] = {1,1,1,2,3,4,5,7,7};
+//	int data[] = {1,2,2};
+	SLinkNode* head = InitSLink();
+	CreateLink(head, data, MaxSize);
+//	ShowList(head);
+	ShowList(DelDub2(head));
+}
