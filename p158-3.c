5bde72e3b9125860fdad3efebc6ce10e1413d0a7
xiaotan415411 <xiaotan@stu.gdou.edu.cn>
2022-10-17 21:36:06 +0800

    p158-3.c

---
+++ p158-3.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#define MaxSize 2
+
+double data[MaxSize];
+double CalcAvg(double data[],int i){
+	double avg = 0;
+	if(i == 0)
+		return data[0] / MaxSize;
+	else{
+		avg += CalcAvg(data,i-1);
+		return avg + data[i] / MaxSize;
+	}
+}
+
+int main(){
+	for(int i = 0;i < MaxSize;i++)
+		data[i] = i + 1;
+	printf("%f\n",CalcAvg(data,MaxSize));
+}
