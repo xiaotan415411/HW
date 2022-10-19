a40dce60dc3d96ed8a4b97050835fc7e01dfd7fc
real_123sb <xiaotan@stu.gdou.edu.cn>
2022-10-19 15:51:38 +0800

    p158-8

---
+++ p158-8.c
@@ -0,0 +1,46 @@
+#include<stdio.h>
+#include<stdlib.h>
+#define MaxSize 5
+
+typedef int ElemType;
+typedef struct SNode {
+	ElemType data;
+	struct SNode* next;
+} SLinkNode;
+
+void CreateLink(SLinkNode* head, int* data, int amount) {
+	SLinkNode* p = head;
+	for (int i = 1; i < amount; i++) {
+		SLinkNode* s = (SLinkNode*)malloc(sizeof(SLinkNode));
+		s->data = data[i];
+		s->next = 0;
+		p->next = s;
+		p = s;
+	}
+}
+
+void traverseL(SLinkNode* L){
+    SLinkNode* p = L;
+    if(p->next == 0){
+        printf("%d ",p->data);
+        return;
+    }else{
+        traverseL(p->next);
+        printf("%d ",p->data);
+        return;
+    }
+}
+
+ElemType data[MaxSize];
+void InitData(ElemType* data) {
+	for (int i = 0; i < MaxSize; i++)
+		data[i] = i;
+}
+
+int main(){
+    SLinkNode link;
+    InitData(data);
+    link.data = data[0];
+    CreateLink(&link,data,MaxSize);   //创建不带头节点单链表
+    traverseL(&link);
+}
